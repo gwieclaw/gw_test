acac46e21bd0d831d19ad9dd848b8342b2b64f59
gwieclaw <some@mail.ru>
2012-06-30 16:24:34 +0200

    6 branch

--- GitTesting/src/com/gw/Runner.java
+++ GitTesting/src/com/gw/Runner.java
@@ -2,6 +2,6 @@ package com.gw;
 
 public class Runner {
 	public static void main(String[] args) {
-		System.out.println("Hello world! 5 branch");
+		System.out.println("Hello world! 6 branch");
 	}
 }
