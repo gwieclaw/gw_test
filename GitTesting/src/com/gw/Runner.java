f541a7d173d1872a244b93af901617e0a3800929
gwieclaw <some@mail.ru>
2012-06-30 16:29:28 +0200

    7 commit

--- GitTesting/src/com/gw/Runner.java
+++ GitTesting/src/com/gw/Runner.java
@@ -2,6 +2,6 @@ package com.gw;
 
 public class Runner {
 	public static void main(String[] args) {
-		System.out.println("Hello world! 5 master");
+		System.out.println("Hello world! 7 branch");
 	}
 }
